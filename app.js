efd02894e00f8163f24f81877b7f9390f71c5368
Yamini Verma <yamini@unibuddy.com>
2021-04-07 00:45:52 +0530

    Added search city filter and pagination

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let cities = [];
 let filteredCities = [];
 let currPage = 0;
 let selectedTypes = [];
+let totalPages = 0;
+let searchCityString = "";
+let pagedFilteredCities = [];
 
 async function getWeatherDetails() {
     let response = await fetch("https://raw.githubusercontent.com/Lokenath/JSON_DATA/master/data.json");
@@ -18,20 +21,21 @@ async function getWeatherDetails() {
 function init() {
     getWeatherDetails().then((data) => {
         weatherData = data;
-        renderPage(weatherData)
+        cities = weatherData.cities;
+        filteredCities = cities;
+        temperatureColor = weatherData.metadata.temperatureColor;
+        pageSize = weatherData.metadata.pageSize;
+        unit = weatherData.metadata.unit;
+        weatherType = weatherData.metadata.weatherType;
+        totalPages = Math.ceil(filteredCities.length / pageSize);
+        renderPage();
     });
-    console.log("weatherData: ", weatherData);
 }
 
-function renderPage(weatherData) {
-    console.log("fetched weatherData: ", weatherData);
-    cities = weatherData.cities;
-    temperatureColor = weatherData.metadata.temperatureColor;
-    pageSize = weatherData.metadata.pageSize;
-    unit = weatherData.metadata.unit;
-    weatherType = weatherData.metadata.weatherType;
+function renderPage() {
     let banner = document.getElementById("banner");
     let weatherTypeDiv = document.createElement("div");
+    weatherTypeDiv.classList.add("checkbox-div");
     let weatherTypeInnerHTML = '';
     weatherType.map((type) => {
         selectedTypes.push(type.toLowerCase());
@@ -39,40 +43,57 @@ function renderPage(weatherData) {
     });
     weatherTypeDiv.innerHTML = weatherTypeInnerHTML;
     banner.appendChild(weatherTypeDiv);
+    filterCities();
+    setPagination();
+}
+
+function setPagination() {
+    totalPages = Math.ceil(filteredCities.length / pageSize);
+    const nextPageBtn = document.getElementById("next");
+    const prevPageBtn = document.getElementById("prev");
+    if (totalPages === 0) {
+        nextPageBtn.disabled = true;
+        prevPageBtn.disabled = true;
+    } else {
+        if (currPage === totalPages - 1) {
+            nextPageBtn.disabled = true;
+        } else {
+            nextPageBtn.disabled = false;
+        }
+        if (currPage === 0) {
+            prevPageBtn.disabled = true;
+        } else {
+            prevPageBtn.disabled = false;
+        }
+    }
+    pagedFilteredCities = filteredCities.slice(currPage*pageSize, (currPage+1)*pageSize);
     renderWeatherCards();
 }
 
 function onSelectWeatherType(cb) {
-    console.log("came in sunny event handler")
     console.log(cb);
-    console.log("id: ", cb.id);
-    console.log("selectedTypes: ", selectedTypes);
     if (weatherType.find(type => type.toLowerCase() === cb.id)) {
         if (cb.checked) {
             selectedTypes.push(cb.id);
         } else {
             selectedTypes = selectedTypes.filter(type => type.toLowerCase() !== cb.id);
         }
-        renderWeatherCards();
+        filterCities();
     }
 }
 
 function renderWeatherCards() {
     const weatherCardsDiv = document.querySelector(".weather-container");
-    console.log("weatherCardsDiv: ", weatherCardsDiv);
     if (weatherCardsDiv) {
         weatherCardsDiv.remove();
     }
-    console.log("came in render weather cards");
-    console.log("render selectedTypes: ", selectedTypes);
-    filteredCities = cities.filter(city => selectedTypes.includes(city.type.toLowerCase())).slice(currPage*pageSize, (currPage+1)*pageSize);
-    console.log("filteredCities: ", filteredCities);
+    const paginationDiv = document.getElementById("pagination-div");
     const outerContainer = document.getElementById("outer-container");
     let weatherContainer = document.createElement("div");
     weatherContainer.classList.add("weather-container");
-    let weatherCards = filteredCities.map(city => renderWeatherCard(city));
+    let weatherCards = pagedFilteredCities.map(city => renderWeatherCard(city));
     weatherContainer.append(...weatherCards);
-    outerContainer.appendChild(weatherContainer);
+    outerContainer.insertBefore(weatherContainer, paginationDiv);
 }
 
 
@@ -84,5 +105,32 @@ function renderWeatherCard(city) {
     card.innerHTML = `<div class="city-name">${city.name}</div><div class="temperature-div"><div class="temp-div" style="color: ${color.color}">${city.temperature}</div><div class="degree-div">C</div></div><div class="weather-type">${city.type}</div>`;
     return card;
 }
+
+function filterCities() {
+    currPage = 0;
+    filteredCitiesByName = cities;
+    if (searchCityString !== "") {
+        filteredCitiesByName = cities.filter(city => (city.name.toLowerCase()).includes(searchCityString));
+    }
+    filteredCities = filteredCitiesByName.filter(city => selectedTypes.includes(city.type.toLowerCase()));
+    pagedFilteredCities = filteredCities.slice(currPage*pageSize, (currPage+1)*pageSize);
+    setPagination();
+    renderWeatherCards();
+}
+
+document.getElementById("search-cities").addEventListener("keyup", (event) => {
+    searchCityString = event.target.value;
+    filterCities();
+});
+
+document.querySelector("#next").addEventListener("click", (event) => {
+    currPage += 1;
+    setPagination();
+});
+
+document.querySelector("#prev").addEventListener("click", (event) => {
+    currPage -= 1;
+    setPagination();
+});
 
 init();
